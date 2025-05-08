33f47c35d5a98e8183eb09d814b0e9bbe4cd45a5
CrisF35 <cristopheromg35@gmail.com>
2025-05-08 16:20:10 -0500

    Segunda Subida

--- validaciones.js
+++ validaciones.js
@@ -4,10 +4,12 @@ document.getElementById('clienteForm').addEventListener('submit', function(event
     const apellido = document.getElementById('apellido').value.trim();
     const correo = document.getElementById('correo').value.trim();
     const telefono = document.getElementById('telefono').value.trim();
+    const direccion = document.getElementById('direccion').value.trim();
+    const fechaNacimiento = document.getElementById('date').value;
     const errorMsg = document.getElementById('errorMsg');
 
     // Validaciones básicas
-    if (!nombre || !apellido || !correo || !telefono) {
+    if (!nombre || !apellido || !correo || !telefono || !direccion || !fechaNacimiento) {
       errorMsg.textContent = 'Todos los campos son obligatorios.';
       return;
     }
@@ -19,6 +21,15 @@ document.getElementById('clienteForm').addEventListener('submit', function(event
 
     if (!/^\d{7,15}$/.test(telefono)) {
       errorMsg.textContent = 'El teléfono debe contener solo números (7-15 dígitos).';
+      return;
+    }
+
+    const fechaIngresada = new Date(fechaNacimiento);
+    const hoy = new Date();
+    hoy.setHours(0, 0, 0, 0); // Para comparar solo la fecha
+
+    if (fechaIngresada > hoy) {
+      errorMsg.textContent = 'La fecha de nacimiento no puede ser en el futuro.';
       return;
     }
 
